613095e6c5c555d6568d77aa4cd23f989b132dec
Leszek8915 <79628901+Leszek8915@users.noreply.github.com>
2024-12-21 19:31:55 +0000

    dod

--- zajecia_2/bankomat/bankomat.cpp
+++ zajecia_2/bankomat/bankomat.cpp
@@ -9,85 +9,112 @@ const double STAN_KONTA = 1000.0;
 
 bool sprawdz_PIN(int wprowadzony_PIN)
 {
-   return wprowadzony_PIN == PIN;
+    return wprowadzony_PIN == PIN;
+}
+
+bool zmien_PIN()
+{
+    int wprowadzony_PIN;
+    cout << "Prosze podac PIN: ";
+    cin >> wprowadzony_PIN;
+    if (sprawdz_PIN(wprowadzony_PIN))
+    {
+        
+        cout << "Prosze podac nowy PIN"
+        cin >> wprowadzony_PIN;
+    }
 }
 
 void pokaz_stan_konta(double stan_konta)
 {
-   cout<<endl<<"Aktualny stan konta: "<<stan_konta<<" PLN"<<endl;
+    cout << endl
+         << "Aktualny stan konta: " << stan_konta << " PLN" << endl;
 }
 
-double wybierz_pieniadze(double stan_konta, double kwota) {
-if (kwota > stan_konta) {
-cout<<"Nie ma wystarczająco dużo środków na koncie. Proszę wybrać kwotę mniejszą bądź równą "<<stan_konta<<"."<<endl;
-} else {
-   return stan_konta;
-}
-return 0;
+double wybierz_pieniadze(double stan_konta, double kwota)
+{
+    if (kwota > stan_konta)
+    {
+        cout << "Nie ma wystarczająco dużo środków na koncie. Proszę wybrać kwotę mniejszą bądź równą " << stan_konta << "." << endl;
+    }
+    else
+    {
+        return stan_konta;
+    }
+    return 0;
 }
 
 double wplac_pieniadze(double stan_konta, double kwota)
 {
-stan_konta += kwota;
-cout<<"Wplacono "<<kwota<<" PLN"<<endl;
-return stan_konta;
+    stan_konta += kwota;
+    cout << "Wplacono " << kwota << " PLN" << endl;
+    return stan_konta;
 }
 
+int main()
+{
+    int wprowadzony_PIN;
+    double stan_konta = STAN_KONTA;
+    int wybor;
+    double kwota;
+    int proby = 3;
+    while (proby > 0)
+    {
+        cout << "Prosze podac PIN: ";
+        cin >> wprowadzony_PIN;
 
-int main() {
-int wprowadzony_PIN;
-double stan_konta = STAN_KONTA;
-int wybor;
-double kwota;
-int proby = 3;
-while (proby > 0) {
-cout<<"Prosze podac PIN: ";
-cin>>wprowadzony_PIN;
-
-if (sprawdz_PIN(wprowadzony_PIN)) {
-break;
-} else {
-proby--;
-if (proby > 0) {
-cout<<"Podany PIN nie jest poprawny. Pozostało prób: "<<proby<<endl;
-} else {
-cout<<"Wykorzystane zostały wszystkie 3 podejścia. Bankomat tymczasowo zablokowany. Proszę jeszcze raz uruchomić program."<<endl;
-return 0;
+        if (sprawdz_PIN(wprowadzony_PIN))
+        {
+            break;
+        }
+        else
+        {
+            proby--;
+            if (proby > 0)
+            {
+                cout << "Podany PIN nie jest poprawny. Pozostało prób: " << proby << endl;
+            }
+            else
+            {
+                cout << "Wykorzystane zostały wszystkie 3 podejścia. Bankomat tymczasowo zablokowany. Proszę jeszcze raz uruchomić program." << endl;
+                return 0;
+            }
+        }
+    }
+    do
+    {
+        cout << "1. Chcę zobaczyć aktualny stan konta" << endl;
+        cout << "2. Chcę wybrać pieniądze" << endl;
+        cout << "3. Chcę wpłacić pieniądze" << endl;
+        cout << "4. Chcę zmienić kod PIN" << endl;
+        cout << "5. Zakończ program" << endl;
+        cout << "Wybierz jedną z dostępnych opcji: ";
+        cin >> wybor;
+        switch (wybor)
+        {
+        case 1:
+            pokaz_stan_konta(stan_konta);
+            break;
+        case 2:
+            cout << "Proszę podać kwotę do wypłaty: ";
+            cin >> kwota;
+            stan_konta = wybierz_pieniadze(stan_konta, kwota);
+            break;
+        case 3:
+            cout << "Proszę podać kwotę do wpłaty: ";
+            cin >> kwota;
+            stan_konta = wplac_pieniadze(stan_konta, kwota);
+            break;
+        case 4:
+        cout << "Podaj dotychczasowy kod PIN" case 5:
+            cout << "Dziękuję za skorzystanie z bankomatu." << endl;
+            break;
+        default:
+            cout << "Nieprawidłowa opcja, proszę wybrać jeszcze raz (1-4)." << endl;
+        }
+    } while (wybor != 4);
+    return 0;
 }
-}
-}
-do {
-cout<<"1. Chcę zobaczyć aktualny stan konta"<<endl;
-cout<<"2. Chcę wybrać pieniądze"<<endl;
-cout<<"3. Chcę wpłacić pieniądze"<<endl;
-cout<<"4. Zakończ program"<<endl;
-cout<<"Wybierz jedną z dostępnych opcji: ";
-cin>>wybor;
-switch (wybor) {
-case 1:
-pokaz_stan_konta(stan_konta);
-break;
-case 2:
-cout<<"Proszę podać kwotę do wypłaty: ";
-cin>>kwota;
-stan_konta = wybierz_pieniadze(stan_konta, kwota);
-break;
-case 3:
-cout<<"Proszę podać kwotę do wpłaty: ";
-cin>>kwota;
-stan_konta = wplac_pieniadze(stan_konta, kwota);
-break;
-case 4:
-cout<<"Dziękuję za skorzystanie z bankomatu."<<endl;
-break;
-default:
-cout<<"Nieprawidłowa opcja, proszę wybrać jeszcze raz (1-4)."<<endl;
-}
-} while (wybor != 4);
-return 0;
-}
-
-
 
 /*Opis działania:
 Przedstawiony program został napisany w języku C++ i ma za zadanie imitować
