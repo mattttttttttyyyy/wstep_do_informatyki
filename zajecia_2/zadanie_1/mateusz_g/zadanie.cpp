a98bbaea0b4df89f10aca6c7dc490dce373ef439
Mateusz G <130585990+mattttttttttyyyy@users.noreply.github.com>
2024-12-21 18:19:22 +0000

    dodano mapę punktów

--- zajecia_2/zadanie_1/mateusz_g/zadanie.cpp
+++ zajecia_2/zadanie_1/mateusz_g/zadanie.cpp
@@ -4,6 +4,13 @@
 
 // This array's length is equal to english alphabet's length (0-25)
 const int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
+const std::unordered_map<char, int> POINTS_MAP = {
+    {'A', 1}, {'B', 3}, {'C', 3}, {'D', 2}, {'E', 1}, 
+    {'F', 4}, {'G', 2}, {'H', 4}, {'I', 1}, {'J', 8}, 
+    {'K', 5}, {'L', 1}, {'M', 3}, {'N', 1}, {'O', 1}, 
+    {'P', 3}, {'Q', 10}, {'R', 1}, {'S', 1}, {'T', 1}, 
+    {'U', 1}, {'V', 4}, {'W', 4}, {'X', 8}, {'Y', 4}, {'Z', 10}
+};
 
 int computeScore(char* word);
 void printGameTitle();
